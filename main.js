3c7c89057393afafbfe81f5c0409a6bb237afac8
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-03-20 22:50:50 -0400

    complete

--- main.js
+++ main.js
@@ -2,10 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
 
 
+var liker = document.getElementsByClassName("like-glyph")
+
+console.log (liker)
+function likeCallBack(event){
+  const heart = event.target
+mimicServerCall("url.com")
+.then((response)=> {
+  if(heart.innerText == EMPTY_HEART){
+    heart.innerText = FULL_HEART
+heart.className = "activated-heart"
+  }
+else {
+  heart.innerText = EMPTY_HEART
+heart.className = ""
+}
+})
+.catch((error)=>{
+  document.getElementById("modal").className = ""
+  document.getElementById("modal").innerText = error
+  setTimeout(()=>document.getElementById("modal").className = "hidden", 3000)
+})
+}
+for (let i = 0; i < liker.length; i++){
+  liker[i].addEventListener("click", likeCallBack)
+}  
+
+
+//
+// mimicServerCall("url.com")
+//   .then((response) => ({
+//     document.GetElementsByClassName("like-glyph")})
+//   .catch((err)=>{document.getElementById("modal").classList.remove("hidden")})
+  
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
